8580da91445bb8de3240bcea7e9ca63565ebf4ff
Akira Matsuda <akira.m.itachi@gmail.com>
2013-08-10 04:01:57 +0900

    bug fix

--- SDK/UI/HTBHatenaBookmarkActivity.m
+++ SDK/UI/HTBHatenaBookmarkActivity.m
@@ -67,6 +67,13 @@
 			[[HTBHatenaBookmarkManager sharedManager] authorizeWithLoginUserInterface:self.presentingViewController success:^{
 				NSLog(@"login success");
 				[self performActivity];
+				double delayInSeconds = 1.0;
+				dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+				dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+					HTBHatenaBookmarkViewController *viewController = [[HTBHatenaBookmarkViewController alloc] init];
+					viewController.URL = url;
+					[self.presentingViewController presentViewController:viewController animated:YES completion:nil];
+				});
 			} failure:^(NSError *error) {
 				NSLog(@"%@", error.localizedDescription);
 			}];
