4d6ac7e4a0abd856caa1db9897e9acf12eba7330
Akira Matsuda <akira.m.itachi@gmail.com>
2013-08-21 17:14:50 +0900

    シェアボタンの状態を常にONにする

--- DemoApp/DemoApp/HTBDemoViewController.m
+++ DemoApp/DemoApp/HTBDemoViewController.m
@@ -101,7 +101,7 @@
     [[HTBHatenaBookmarkManager sharedManager] logout];
     [[HTBHatenaBookmarkManager sharedManager] authorizeWithSuccess:^{
         [self toggleLoginButtons];
-        self.navigationItem.rightBarButtonItem.enabled = [HTBHatenaBookmarkManager sharedManager].authorized;
+//        self.navigationItem.rightBarButtonItem.enabled = [HTBHatenaBookmarkManager sharedManager].authorized;
     } failure:^(NSError *error) {
     }];
 }
@@ -110,7 +110,7 @@
 {
     [[HTBHatenaBookmarkManager sharedManager] logout];
     [self toggleLoginButtons];
-    self.navigationItem.rightBarButtonItem.enabled = [HTBHatenaBookmarkManager sharedManager].authorized;
+//    self.navigationItem.rightBarButtonItem.enabled = [HTBHatenaBookmarkManager sharedManager].authorized;
 }
 
 - (void)initializeHatenaBookmarkClient {
